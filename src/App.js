e5beca287551b1870960ea62676c3d2c6b9a820f
MicleJohn369 <miclej369@gmail.com>
2023-05-22 18:37:07 +0300

    feat: add component page PostPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import PostPage from "./pages/PostPage";
 
 function App() {
   const homepage = process.env.PUBLIC_URL;
   return (
     <div className="App">
       <Switch>
-        <Route path={homepage} component={Homepage} />
+        <Route path={homepage} exact component={Homepage} />
+        <Route path={`${homepage}/login`} component={Login} />
+        <Route path={`${homepage}/post/:pageId`} component={PostPage} />
       </Switch>
     </div>
   );
